ec0fd1ba2eb4518b1754889956e7168fc8d41af9
CuamckuyKot <CuamckuyKot@Super>
2011-03-07 12:29:36 +0300

    Add missing files.

---
+++ sites/default/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'database' => 'mysql://root@localhost/cogear',
+    'cache' => array(
+        'adapter' => Cache::FILE,
+        'enabled' => TRUE,
+        'path' => SITE . DS . 'cache',
+    ),
+    // Secret key
+    'key' => 'asdlmk1;2u3192y23uhsdhhabgkjahsdgkjghasd',
+    'development' => TRUE,
+);
+
