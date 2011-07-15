6fba8030f8fedfd558192b791386b6009a4f872d
Dmitriy Belyaev <admin@cogear.ru>
2011-07-16 00:43:27 +0400

    Fix Image classes to work properly into new files structure.

---
+++ engine/Image/Gear.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Image gear
+ *
+ * @author		Dmitriy Belyaev <admin@cogear.ru>
+ * @copyright		Copyright (c) 2011, Dmitriy Belyaev
+ * @license		http://cogear.ru/license.html
+ * @link		http://cogear.ru
+ * @package		Image
+ * @subpackage          
+ * @version		$Id$
+ */
+class Image_Gear extends Gear {
+    protected $name = 'Image';
+    protected $description = 'Image processor.';
+    protected $package = 'Images';
+}
