5be79cfcf4dcbd38d5d5205a265b609fefb3a550
Dmitriy Belyaev <admin@cogear.ru>
2011-07-19 17:29:49 +0400

    Great update. Make a big sitep in the way to social networking platform.

--- engine/Admin/Theme/js/admin.js
+++
@@ -1,7 +0,0 @@
-(function($){
-    $(document).ready(function(){
-      $('table.list .check-all input[type=checkbox]').click(function(){
-          $(this).parent().parent().parent().parent().find('input[type=checkbox]').attr('checked',$(this).attr('checked'));
-      }); 
-    });
-})(jQuery);
