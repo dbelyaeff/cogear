dbea8a3c0c0f65f03c3a074a57a419ce6e36d1db
Dmitriy Belyaev <admin@cogear.ru>
2011-08-02 00:47:56 +0400

    Fix mistake.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,16 +1,20 @@
 <?php
-
 return array(
     'database' => array(
-      'dsn'=> 'mysql://root@localhost/cogear'
-      ),
+        'dsn' => 'mysqli://root@localhost/cogear',
+    ),
+    'permitted_uri_chars' => 'а-яa-z0-9\s_\.',
     'cache' => array(
-        'adapter' => Cache::FILE,
+        'adapter' => 'Cache_Adapter_File',
         'enabled' => TRUE,
         'path' => SITE . DS . 'cache',
     ),
     // Secret key
     'key' => 'asdlmk1;2u3192y23uhsdhhabgkjahsdgkjghasd',
     'development' => TRUE,
+    'site' => array(
+        'name' => 'cogear',
+        'locale' => 'ru',
+    ),
 );
 
