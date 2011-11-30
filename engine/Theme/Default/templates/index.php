453b75372ae54fd7737a73ca4797640454d0e4bc
Aleksandr Naumov <inetlover@gmail.com>
2011-11-30 20:57:10 +0600

    Changed DOCTYPE to HTML5

--- engine/Theme/Default/templates/index.php
+++ engine/Theme/Default/templates/index.php
@@ -1,6 +1,6 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=$cogear->get('site.lang','en');?>">
-<head>
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
 	<?=theme('head')?>
     </head>
     <body>
