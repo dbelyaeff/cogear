92d04514b8dc0d76090811cd49401d0134e899ab
CuamckuyKot <admin@cogear.ru>
2012-01-04 12:00:48 +0300

    Add conditions and groups. For example, if you want you js or css to be diplayed only on Windows and IE9 you must name it your_script[windows+ie9].js. More complex — your_style[windows+ie9,linux+firefox7].css. And you can even define your own group by your hands — your_style@print.css.

--- engine/Core/Harvester.php
+++ engine/Core/Harvester.php
@@ -21,11 +21,25 @@ class Harvester {
     private $scripts;
 
     /**
+     * Groups of scripts that must be rendered
+     * 
+     * @var array 
+     */
+    public static $scriptsRenderGroups = array('default');
+
+    /**
      * Styles
      *
      * @var Core_ArrayObject
      */
     private $styles;
+
+    /**
+     * Group of stypes that must be rendered
+     * 
+     * @var array
+     */
+    public static $stylesRenderGroups = array('screen');
 
     /**
      * Constructor
@@ -34,8 +48,8 @@ class Harvester {
         $this->clear();
         Template::bindGlobal('scripts', $this->scripts);
         Template::bindGlobal('styles', $this->styles);
-        hook('head', array($this, 'output'),NULL,'css');
-        hook('after', array($this, 'output'),NULL,'js');
+        hook('head', array($this, 'output'), NULL, 'css');
+        hook('after', array($this, 'output'), NULL, 'js');
     }
 
     /**
@@ -45,10 +59,112 @@ class Harvester {
      * @param   string  $group
      */
     public function addScript($path, $group = 'default') {
-        if (Filesystem::getExtension($path) == 'js') {
-            isset($this->scripts->$group) OR $this->scripts->$group = new Core_ArrayObject();
-            $this->scripts->$group->$path = HTML::script(self::preparePath($path));
+        $this->add($path, $group);
+    }
+
+    /**
+     * Add script or style
+     * 
+     * @param string $path
+     * @param string $type 
+     */
+    private function add($path, $type) {
+        $ext = Filesystem::getExtension($path);
+        switch ($ext) {
+            case 'js':
+                $method = 'attachScript';
+                break;
+            case 'css':
+                $method = 'attachStyle';
+                break;
         }
+        $special_types = self::checkConditions($path);
+        if (FALSE === $special_types) {
+            if ($new_type = self::defineGroupFromFilename($path)) {
+                if (is_array($new_type)) {
+                    foreach ($new_type as $type) {
+                        $this->$method($path, $type);
+                    }
+                }
+            } else {
+                $this->$method($path, $type);
+            }
+        } else {
+            foreach ($special_types as $type) {
+                if ($ext == 'js') {
+                    self::$scriptsRenderGroups[] = $type;
+                } elseif ($ext == 'css') {
+                    self::$stylesRenderGroups[] = $type;
+                }
+                $this->$method($path, $type);
+            }
+        }
+    }
+
+    /**
+     * Attach script to some group
+     * 
+     * @param string $path
+     * @param string $group 
+     */
+    private function attachScript($path, $group) {
+        isset($this->scripts->$group) OR $this->scripts->$group = new Core_ArrayObject();
+        $this->scripts->$group->$path = HTML::script(self::preparePath($path));
+    }
+
+    /**
+     * This method simple look througth the file name and find snippets for groups
+     * 
+     * Example: style@screen.css, style@print.css, style@screen+print.css
+     * 
+     * @param string $path 
+     * @return  boolean|array
+     */
+    public static function defineGroupFromFilename($path) {
+        $filename = pathinfo($path, PATHINFO_FILENAME);
+        if (preg_match("#@(.+)$#", $filename, $matches)) {
+            return $result = explode(',', $matches[1]);
+        }
+        return FALSE;
+    }
+
+    /**
+     * Check adding file for special contidions
+     * 
+     * main[firefox7,ie9].js, super_stype[windows+ie9,android,linux+firefox]
+     * 
+     * @param type $path
+     * @return type 
+     */
+    public static function checkConditions($path) {
+        $filename = pathinfo($path, PATHINFO_FILENAME);
+        if (preg_match("#\[([^\]]+)\]#", $filename, $matches)) {
+// Get list of conditions
+// For example: windows+ie_9,linux+firefox
+            $conditions = explode(',', $matches[1]);
+            $ua = cogear()->request->getUserAgent();
+            $types = array();
+            foreach ($conditions as $condition) {
+                $subconditions = explode('+', $condition);
+                $result = FALSE;
+                foreach ($subconditions as $subcondition) {
+                    if ($subcondition == $ua['browser'] OR
+                            $subcondition == $ua['browser'] . $ua['version'] OR
+                            $subcondition == $ua['os'] OR
+                            $subcondition == 'mobile' && !is_null($ua['is_mobile']) OR
+                            $subcondition == $ua['locale']) {
+                        $result = TRUE;
+                    } else {
+                        $result = FALSE;
+                    }
+                }
+                if ($result) {
+                    $types[] = $condition;
+                }
+            }
+            return $types;
+        }
+        return FALSE;
     }
 
     /**
@@ -71,10 +187,18 @@ class Harvester {
      * @param   string  $path
      */
     public function addStyle($path, $media = 'screen') {
-        if (Filesystem::getExtension($path) == 'css') {
-            isset($this->styles->$media) OR $this->styles->$media = new Core_ArrayObject();
-            $this->styles->$media->$path = HTML::style(self::preparePath($path), array('media' => $media));
-        }
+        $this->add($path, $media);
+    }
+
+    /**
+     * Attach style
+     * 
+     * @param string $path
+     * @param string $media 
+     */
+    private function attachStyle($path, $media) {
+        isset($this->styles->$media) OR $this->styles->$media = new Core_ArrayObject();
+        $this->styles->$media->$path = HTML::style(self::preparePath($path), array('media' => $media));
     }
 
     /**
@@ -108,10 +232,18 @@ class Harvester {
     /**
      * Get scripts
      *
+     * @string  $group
      * @return string
      */
-    public function getScripts($group = 'default') {
-        return $this->scripts->$group->__toString() . "\n";
+    public function getScripts($group = NULL) {
+        if ($group) {
+            return $this->scripts->$group->__toString() . "\n";
+        }
+        $output = '';
+        foreach (self::$scriptsRenderGroups as $group) {
+            $output .= $this->scripts->$group->__toString() . "\n";
+        }
+        return $output;
     }
 
     /**
@@ -119,18 +251,23 @@ class Harvester {
      *
      * @return string
      */
-    public function getStyles($media = 'screen') {
-        // Sort styles. Theme styles must be on the top.
-        $styles = $this->styles->$media->toArray();
+    public function getStyles($media = NULL) {
+        if ($media) {
+            $styles = $this->styles->$media->toArray();
+        } else {
+            $styles = array();
+            foreach (self::$stylesRenderGroups as $group) {
+                $styles = array_merge($styles, $this->styles->$group->toArray());
+            }
+        }
         $theme_styles = array();
-        foreach($styles as $key=>$value){
-            if(strpos($key,'themes') !== FALSE){
+        foreach ($styles as $key => $value) {
+            if (strpos($key, 'themes') !== FALSE) {
                 $theme_styles[$key] = $value;
                 unset($styles[$key]);
             }
         }
-        $this->styles->$media->exchangeArray(array_merge($theme_styles,$styles));
-        return $this->styles->$media->__toString() . "\n";
+        return implode("\n", array_merge($theme_styles, $styles));
     }
 
     /**
