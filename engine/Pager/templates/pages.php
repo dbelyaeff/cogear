9c6d89619063108fe64b112354cc8734fb0709ea
Aleksandr Naumov <inetlover@gmail.com>
2012-01-16 00:47:48 +0600

    Updated styles for the gear Pager on Bootstrap v1.4.0

--- engine/Pager/templates/pages.php
+++ engine/Pager/templates/pages.php
@@ -1,28 +1,18 @@
-<div class="pager<?if($ajaxed):?> ajaxed<?endif?>"<?if($target):?> rel="<?=$target?>"<?endif?>>
-    <div class="controls">
+<div class="pagination<?if($ajaxed):?> ajaxed<?endif?>"<?if($target):?> rel="<?=$target?>"<?endif?>>
+    <ul>
         <?php
-        if ($current != $first) {
-            echo '← <a class="prev" href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $prev)) : $prev) . '">' . t('previous', 'Pager') . '</a>';
-        }
-        ?>
-
-        <?php
-        if ($current != $last) {
-            echo '<a class="next" href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $next)) : $next) . '">' . t('next', 'Pager') . '</a> →';
-        }
-        ?>
-    </div>
-    <ul class="pages">
-        <?php
-        if ($current != $first) {
-            echo '<li class="first"><a href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $first)) : $first) . '">' . t('←', 'Pager') . '</a></li>';
-        }
+        $disabled ='';
+        if ($current == $first) {
+				$disabled ='disabled';
+			}
+            echo '<li class="turn-pages '.$disabled.'"><a href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $first)) : $first) . '">' . t('&laquo;', 'Pager') . '</a></li>';
+            echo '<li class="turn-pages '.$disabled.'"><a href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $prev)) : $prev) . '">' . t('&lsaquo;', 'Pager') . '</a></li>';      
         if ($order == Pager_Pages::FORWARD) {
             for ($i = $first; $i <= $last; $i++) {
                 if ($i != $current) {
                     echo '<li><a href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $i)) : $i) . '">' . $i . '</a></li>';
                 } else {
-                    echo '<li class="current">' . $i . '</li>';
+                    echo '<li class="active"><a href="">' . $i . '</a></li>';
                 }
             }
         } else {
@@ -30,13 +20,16 @@
                 if ($i != $current) {
                     echo '<li><a href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $i)) : $i) . '">' . $i . '</a></li>';
                 } else {
-                    echo '<li class="current">' . $i . '</li>';
+                    echo '<li class="active"><a href="">' . $i . '</a></li>';
                 }
             }
         }
-        if ($current != $last) {
-            echo '<li class="last"><a href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $last)) : $last) . '">' . t('→', 'Pager') . '</a></li>';
-        }
+        $disabled ='';
+        if ($current == $last) { 
+				$disabled ='disabled'; 
+			}
+			echo '<li class="turn-pages '.$disabled.'"><a href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $next)) : $next) . '">' . t('&rsaquo;', 'Pager') . '</a></li>';
+            echo '<li class="turn-pages '.$disabled.'"><a href="' . $base_uri . ($method ? '?' . Url::extendQuery(array(Pager_Pages::ARG => $last)) : $last) . '">' . t('&raquo;', 'Pager') . '</a></li>';
         ?>
     </ul>
 </div>
